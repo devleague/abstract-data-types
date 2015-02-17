aea7aa4ee8739b7a099dd3d8d8f79df51101898e
George Lee <keoki.lee@gmail.com>
2015-02-16 21:59:50 -1000

    Forgot to include in index.
    Forgot to include the map as part of the index exports.

--- index.js
+++ index.js
@@ -2,3 +2,4 @@ exports.Node = require('./lib/Node');
 exports.LinkedList = require('./lib/LinkedList');
 exports.Stack = require('./lib/Stack');
 exports.Queue = require('./lib/Queue');
+exports.Map = require('./lib/Map');
